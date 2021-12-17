a6e90945ea8e9661a3cc889eba1e2bbbe8f167cf
cn8817 <christinenguyen1433@gmail.com>
2021-12-16 20:02:59 -0800

    finished chunked problem

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,18 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    const chunked = [] //create the parent array
+    for(let num of array){ //looping over each number in the array
+        let last = chunked[chunked.length - 1] // last is equal to the last subarray in the array
+        if(!last || last.length === size){ //if there is no subarray or the subarray has met its capacity
+            chunked.push([num]) //push a new subarray with the number we are currently iterating over into the parent array
+        } else{
+            last.push(num) //if a subarray exists and it hasnt met its capacity, push the num into the subarray
+        }
+    }
+    return chunked
+
+}
 
 module.exports = chunk;
