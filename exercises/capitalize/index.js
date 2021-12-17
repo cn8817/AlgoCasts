ebad0c4d9fc55088689231589a8965d1a25c02a3
cn8817 <christinenguyen1433@gmail.com>
2021-12-16 21:08:59 -0800

    finished capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,17 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    const words = [] //make empty array
+
+    for(let word of str.split(' ')){ // loop over each word of the string, split each word at the space so we only have an array of words ex: ['hello', 'there']
+        words.push(word[0].toUppercase() + word.slice(1))
+        // push the changes into the words array
+        // uppercase the first letter of the word (index 0)
+        // slice takes every letter of the word from index 1 onwards
+    }
+
+    return words.join(' ') // join back each word with a space to form a sentence string
+}
 
 module.exports = capitalize;
