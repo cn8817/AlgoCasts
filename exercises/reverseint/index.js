9a3d0eca2d5c392215bc72f91e8ec8015f30790b
cn8817 <christinenguyen1433@gmail.com>
2021-12-16 20:24:53 -0800

    completed reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let reversed = n
+    .toString() //turn the number to a string
+    .replace('-', '') //replaces negative with empty string to remove it completely
+    .split('') //split only works with strings, ex: '51' = ['5', '1'] bc of split
+    .reverse() //reverse the array
+    .join('') //join each array value back to a string
+
+    return parseInt(reversed) * Math.sign(n)
+    // parseInt turns string to a number and Math.sign deals with any negatives
+}
 
 module.exports = reverseInt;
