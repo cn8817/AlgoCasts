783df658bebcc5c354445c78297bdfaa4ce5770f
cn8817 <christinenguyen1433@gmail.com>
2021-12-16 20:29:56 -0800

    finsihed anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,18 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB) //returns true if stringA is the same as stringB
+}
+
+//helper function (do this first)
+function cleanString(str){
+    return str
+    .replace(/[^\w]/g, '') //removes any spaces and punctuations from the string
+    .toLowerCase() //makes string all lowercase
+    .split('') //turns str into array
+    .sort() //sorts array by alphabetical order
+    .join('') //turns array back to string
+}
 
 module.exports = anagrams;
