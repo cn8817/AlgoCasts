04a436ea766cf23d7ff0cd24192acff44f90cf5e
cn8817 <christinenguyen1433@gmail.com>
2021-12-20 14:12:47 -0800

    completed vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,17 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let count = 0 //make a counter that increases everytime there is a vowel
+    const check = ['a', 'e', 'i', 'o', 'u'] //array contains letters that we want to check for
+
+    for(let char of str.toLowerCase()){ //loop over each character, lowercase the string in case of uppercase vowels
+        if (check.includes(char)){ // if the character we are iterationg over is inside the check array
+            count++ //increase counter by 1 every time the condition is met
+        }
+    }
+
+    return count
+}
 
 module.exports = vowels;
